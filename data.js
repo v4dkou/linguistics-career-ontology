1d6b1249f26c9724cf6ce09aa7f8252c832c7f8a
vadkou <vadkou@wave909.com>
2022-09-17 02:55:21 +0700

    Slides for a lecture about careers for linguists

---
+++ data.js
@@ -0,0 +1,134 @@
+data = [
+  {
+    "Group": "Обучение",
+    "Occupation": "Преподаватель (лингвистика)",
+    "Subjects": "Всё",
+    "ESCO": "http://data.europa.eu/esco/occupation/c549f0de-c415-428e-865c-973bcd224b39"
+  },
+  {
+    "Group": "Обучение",
+    "Occupation": "Логопед, дефектолог",
+    "Subjects": "Речевая дефектология, Возрастная лингвистика",
+    "ESCO": "http://data.europa.eu/esco/occupation/8021f3a2-e3de-43c0-b366-075da74dc5b7"
+  },
+  {
+    "Group": "Систематизация знаний",
+    "Occupation": "Лексикограф",
+    "Subjects": "Корпусная лингвистика",
+    "ESCO": "http://data.europa.eu/esco/occupation/49c3db8c-c586-42d1-9726-f65d8303c0b1"
+  },
+  {
+    "Group": "Систематизация знаний",
+    "Occupation": "Инженер по знаниям/таксономист",
+    "Subjects": "Информационный поиск",
+    "ESCO": "http://data.europa.eu/esco/occupation/812c217d-32b7-4f6a-9faf-993d3577ac7f"
+  },
+  {
+    "Group": "Написание текстов",
+    "Occupation": "Технический писатель",
+    "Subjects": "Документирование программного обеспечения, Английский язык, Второй иностранный язык",
+    "ESCO": "http://data.europa.eu/esco/occupation/8cb59bc4-1f39-477e-beea-effed12d186c"
+  },
+  {
+    "Group": "Написание текстов",
+    "Occupation": "Редактор",
+    "Subjects": "Английский язык, Второй иностранный язык",
+    "ESCO": "http://data.europa.eu/esco/occupation/cbdb80db-c760-4b68-b074-88c397650ef5"
+  },
+  {
+    "Group": "Написание текстов",
+    "Occupation": "Копирайтер",
+    "Subjects": "Английский язык, Второй иностранный язык",
+    "ESCO": "http://data.europa.eu/esco/occupation/b1ab3572-fa2f-4624-8eac-3c3d986d237b"
+  },
+  {
+    "Group": "Перевод",
+    "Occupation": "Переводчик",
+    "Subjects": "Английский язык, Второй иностранный язык",
+    "ESCO": "http://data.europa.eu/esco/occupation/1a07bd7d-2e1d-4930-a84a-1a442b8f2a44"
+  },
+  {
+    "Group": "Перевод",
+    "Occupation": "Синхронист",
+    "Subjects": "Английский язык, Второй иностранный язык",
+    "ESCO": "http://data.europa.eu/esco/occupation/23e692a9-a242-4d4f-988f-ed562f757864"
+  },
+  {
+    "Group": "Перевод",
+    "Occupation": "Сурдопереводчик",
+    "Subjects": "Английский язык, Второй иностранный язык, Проектирование и разработка систем компьютерного сурдоперевода для глухих",
+    "ESCO": "http://data.europa.eu/esco/occupation/9a2147aa-a72f-4f83-8653-c3f50d4e1e73"
+  },
+  {
+    "Group": "Нейролингвистика",
+    "Occupation": "Разработчик (биоинформатика)",
+    "Subjects": "Нейролингвистика",
+    "ESCO": "http://data.europa.eu/esco/occupation/68d973df-bf10-4bf7-9a1b-fbd9f604b9db"
+  },
+  {
+    "Group": "Судебная экспертиза",
+    "Occupation": "Судебный эксперт (лингвистика)",
+    "Subjects": "Основы лингвистической экспертизы текста, Социолингвистика, Лексикология",
+    "ESCO": ""
+  },
+  {
+    "Group": "Судебная экспертиза",
+    "Occupation": "Графолог",
+    "Subjects": "",
+    "ESCO": "http://data.europa.eu/esco/occupation/757da94f-d1f3-48ad-ac61-74e22bc18cca"
+  },
+  {
+    "Group": "Разработка ПО",
+    "Occupation": "Разработчик (обработка естественной речи)",
+    "Subjects": "Информатика и основы программирования, Методы и алгоритмы компьютерной лингвистики",
+    "ESCO": "http://data.europa.eu/esco/occupation/4874fa37-0cd1-4a68-aed8-a838851f242d"
+  },
+  {
+    "Group": "Разработка ПО",
+    "Occupation": "Разработчик (компьютерное зрение)",
+    "Subjects": "Информатика и основы программирования,",
+    "ESCO": "http://data.europa.eu/esco/occupation/1c5a45b9-440e-4726-b565-16a952abd341"
+  },
+  {
+    "Group": "Разработка ПО",
+    "Occupation": "Разработчик (\"умные\" системы, AI)",
+    "Subjects": "Информатика и основы программирования, Информационный поиск, Математическая логика",
+    "ESCO": "http://data.europa.eu/esco/occupation/35553663-deab-4d9a-bf22-15c1625d28e8"
+  },
+  {
+    "Group": "Разработка ПО",
+    "Occupation": "Разработчик поисковых систем",
+    "Subjects": "Информатика и основы программирования, Информационный поиск, Базы данных",
+    "ESCO": ""
+  },
+  {
+    "Group": "Разработка ПО",
+    "Occupation": "Разработчик языков программирования",
+    "Subjects": "Информатика и основы программирования, Языки для специальных целей",
+    "ESCO": ""
+  },
+  {
+    "Group": "Разработка ПО",
+    "Occupation": "Разработчик систем сурдоперевода",
+    "Subjects": "Проектирование и разработка систем компьютерного сурдоперевода для глухих",
+    "ESCO": ""
+  },
+  {
+    "Group": "Психолингвистика",
+    "Occupation": "Маркетолог",
+    "Subjects": "Основы лингвистической экспертизы текста, Введение в психологию, Социолингвистика",
+    "ESCO": "http://data.europa.eu/esco/occupation/4791e217-d607-40d5-9f84-df1d0a071cbc"
+  },
+  {
+    "Group": "Психолингвистика",
+    "Occupation": "Политтехнолог",
+    "Subjects": "Основы лингвистической экспертизы текста, Введение в психологию, Социолингвистика",
+    "ESCO": "http://data.europa.eu/esco/occupation/3e213d52-e7a0-4980-99d9-23da1f717f50"
+  },
+  {
+    "Group": "Наука",
+    "Occupation": "Исследователь",
+    "Subjects": "Всё",
+    "ESCO": ""
+  }
+]
